8c8bec1840490652d06c24c54de8f224e543dadf
nicholaikhit <107006971+nicholaikhit@users.noreply.github.com>
2022-06-09 13:45:31 -0700

    Test

--- website/src/modules/components/MuiNavbar.js
+++ website/src/modules/components/MuiNavbar.js
@@ -1,26 +1,30 @@
-import { AppBar, Icon, Toolbar, Typography, useStepperContext, Slide, useScrollTrigger } from "@mui/material"
+import { AppBar, Icon, Toolbar, Typography, useStepperContext, Slide, useScrollTrigger, Stack, Button } from "@mui/material"
 import CurrencyBitcoinIcon from '@mui/icons-material/CurrencyBitcoin';
 
 export const MuiNavbar = () => {
     const trigger = useScrollTrigger();
-    return(
+    return (
         <Slide appear={false} direction='down' in={!trigger}>
             <AppBar color="primary">
-                    <Toolbar>
-                        <CurrencyBitcoinIcon sx={{ display: { xs: 'none', md: 'flex', fontSize: 50}, mr: 1 }} />
-                        <Typography sx={{
-                            mr: 2,
-                            display: { xs: 'none', md: 'flex' },
-                            fontFamily: 'monospace',
-                            fontWeight: 700,
-                            letterSpacing: '.3rem',
-                            color: 'inherit',
-                            textDecoration: 'none',
-                            fontSize: 50,
-                            }}>
-                                ORMI
-                        </Typography>
-                    </Toolbar>
+                <Toolbar>
+                    <CurrencyBitcoinIcon sx={{ display: { xs: 'none', md: 'flex', fontSize: 50 }, mr: 1 }} />
+                    <Typography sx={{
+                        mr: 2,
+                        display: { xs: 'none', md: 'flex' },
+                        fontFamily: 'monospace',
+                        fontWeight: 700,
+                        letterSpacing: '.3rem',
+                        color: 'inherit',
+                        textDecoration: 'none',
+                        fontSize: 50,
+                    }}>
+                        ORMI
+                    </Typography>
+                    <Stack direction='row' spacing={2}>
+                        <Button color='inherit'>Sign In</Button>
+                        <Button color='inherit'>Sign Up</Button>
+                    </Stack>
+                </Toolbar>
             </AppBar>
         </Slide>
     )
